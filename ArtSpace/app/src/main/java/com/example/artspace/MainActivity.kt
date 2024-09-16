d7eab0258f079b5ff1848c899f761c20351eabc1
Xander Lutz <xander.lutz@gmail.com>
2024-09-16 16:25:32 -0500

    Completed Icon Basics, Started Grid, Conformed ArtSpace to rubric

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.artspace
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -15,10 +17,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -29,8 +34,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -71,28 +78,9 @@ fun ArtSpaceApp(modifier: Modifier = Modifier) {
         R.drawable.art_8,
         R.drawable.art_9
     )
-    val titles = arrayOf(
-        R.string.art_1_title,
-        R.string.art_2_title,
-        R.string.art_3_title,
-        R.string.art_4_title,
-        R.string.art_5_title,
-        R.string.art_6_title,
-        R.string.art_7_title,
-        R.string.art_8_title,
-        R.string.art_9_title
-    )
-    val infos = arrayOf(
-        R.string.art_1_info,
-        R.string.art_2_info,
-        R.string.art_3_info,
-        R.string.art_4_info,
-        R.string.art_5_info,
-        R.string.art_6_info,
-        R.string.art_7_info,
-        R.string.art_8_info,
-        R.string.art_9_info
-    )
+
+    val titles = stringArrayResource(R.array.titles)
+    val infos = stringArrayResource(R.array.infos) 
 
     val image = images[state]
     val title = titles[state]
@@ -105,93 +93,86 @@ fun ArtSpaceApp(modifier: Modifier = Modifier) {
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
     ) {
-        Image(
-            painter = painterResource(image),
-            contentDescription = null,
-            contentScale = ContentScale.FillBounds,
-            modifier = Modifier
+
+        Spacer(modifier = Modifier.height(32.dp))
+        Painting(
+            image,
+            title,
+            info,
+            Modifier
                 .width(320.dp)
                 .height(380.dp)
-                .shadow(8.dp)
-        )
-        Spacer(modifier = Modifier.height(64.dp))
-        Label(
-            370,
-            title,
-            info
+                .background(colorResource(R.color.background))
+                .padding(16.dp),
+            Modifier
+                .background(colorResource(R.color.background))
+                .width(360.dp)
+                .padding(16.dp),
         )
         Spacer(modifier = Modifier.height(16.dp))
         Buttons(
-            320,
-            {
-                if (state < 8) {
-                    state += 1
-                }
-                else {
-                    state = 0
-                }
-            },
-            {
-                if (state > 0) {
-                    state -= 1
-                }
-                else {
-                    state = 8
-                }
-            }
+            { if (state < 8) { state += 1 } else { state = 0 } },
+            { if (state > 0) { state -= 1 } else { state = 8 } },
+            Modifier.width(320.dp)
         )
     }
 }
 
 @Composable
-fun Label(
-    width: Int,
-    @StringRes title: Int,
-    @StringRes info: Int,
-    modifier: Modifier = Modifier
+fun Painting(
+    @DrawableRes image: Int,
+    title: String,
+    info: String,
+    imageModifier: Modifier = Modifier,
+    labelModifier: Modifier = Modifier
 ) {
+    Surface(
+        Modifier.wrapContentSize().shadow(8.dp)
+    ) {
+        Image(
+            painter = painterResource(image),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = imageModifier
+        )
+    }
+    Spacer(Modifier.height(32.dp))
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-            .width(width.dp)
-            .background(colorResource(id = R.color.background))
-            .padding(16.dp)
+        modifier = labelModifier
     ) {
         Text(
             fontSize = 24.sp,
-            text = stringResource(title),
+            text = title,
             fontWeight = FontWeight.Bold,
-            modifier = modifier
         )
         Text(
-            text = stringResource(info),
-            modifier = modifier
+            text = info,
         )
     }
 }
 
 @Composable
 fun Buttons(
-    width: Int,
     next: () -> Unit,
     previous: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Row (
         horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = modifier.width(width.dp)
+        modifier = modifier
     ) {
         Button(
             content = {Text( text = stringResource(R.string.previous))},
             onClick = next,
             colors = ButtonDefaults. buttonColors(containerColor = colorResource(id = R.color.black)),
-            modifier = modifier.width(120.dp)
+            modifier = Modifier.width(120.dp)
         )
         Button(
             content = { Text( text = stringResource(R.string.next)) },
             onClick = previous,
             colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.black)),
-            modifier = modifier.width(120.dp)
+            modifier = Modifier.width(120.dp)
         )
     }
 }
