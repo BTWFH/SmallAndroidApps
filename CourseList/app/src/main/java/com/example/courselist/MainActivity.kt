a6dbba29cf0a7f664b59cc8506d666d7d4017d82
Xander Lutz <xander.lutz@gmail.com>
2024-09-16 20:20:21 -0500

    Finished course list

--- CourseList/app/src/main/java/com/example/courselist/MainActivity.kt
+++ CourseList/app/src/main/java/com/example/courselist/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.courselist.data.DataSource
+import com.example.courselist.model.Topic
 import com.example.courselist.ui.theme.CourseListTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +33,61 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CourseListTheme {
+                TopicApp()
             }
         }
     }
 }
 
+@Composable
+fun TopicApp() {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        DataSource.topics.forEach { topic -> item { TopicCard(topic) } }
+    }
+}
+
+@Composable
+fun TopicCard(
+    topic: Topic,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+    ) {
+        Row {
+            Image(
+                painter = painterResource(topic.image),
+                contentDescription = null,
+                modifier = Modifier.size(68.dp)
+            )
+            Column {
+                Text(
+                    text = stringResource(topic.name),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 8.dp)
+                )
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier.padding(start = 16.dp, end = 8.dp)
+                    )
+                    Text(text = topic.id.toString())
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopicAppPreview() {
+    TopicApp()
+}
---
+++ CourseList/app/src/main/java/com/example/courselist/model/Topic.kt
@@ -0,0 +1,7 @@
+package com.example.courselist.model
+
+data class Topic(
+    val name: Int,
+    val id: Int,
+    val image: Int
+)
